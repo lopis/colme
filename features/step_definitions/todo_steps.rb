ba51078cf473b0441af23bc5ef5807255d69f4b4
João Lopes <joaopclopes@gmail.com>
2014-11-17 16:35:10 +0000

    More tests

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -1,22 +1,50 @@
-Given(/^I am on the home page$/) do
-  visit "demo/smallTableDemo.html"
+module CapybaraExtension
+  def drag_by(right_by, down_by)
+    base.drag_by(right_by, down_by)
+  end
 end
 
-When(/^I see the column (.*) and the column (.*)$/) do |name1, name2|
-  find('#cm-table1 .cm-th', :text => name1)
-  find('#cm-table1 .cm-th', :text => name2)
+module CapybaraSeleniumExtension
+  def drag_by(right_by, down_by)
+    driver.browser.action.drag_and_drop_by(native, right_by, down_by).perform
+  end
+end
+
+::Capybara::Selenium::Node.send :include, CapybaraSeleniumExtension
+::Capybara::Node::Element.send :include, CapybaraExtension
+
+Given(/^I am on the home page$/) do
+  visit "demo/smallTableDemo.html"
 end
 
 When(/^I drag the column (.*) to the column (.*)$/) do |name1, name2|
   find('#cm-table1 .cm-th', :text => name1).drag_to(find('#cm-table1 .cm-th', :text => name2))
 end
 
-When(/^I release the mouse button$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I drag the column (.*) (\d+)px to (.*)$/) do |name, pixels, direction|
+  if direction == 'left'
+    find('#cm-table1 .cm-th', :text => name).drag_by(0-pixels.to_i,0)
+  elsif direction == 'right'
+    find('#cm-table1 .cm-th', :text => name).drag_by(pixels,0)
+  elsif direction == 'up'
+    find('#cm-table1 .cm-th', :text => name).drag_by(0,pixels)
+  elsif direction == 'down'
+    find('#cm-table1 .cm-th', :text => name).drag_by(0,0-pixels.to_i)
+  end
 end
 
-Then(/^I should see the column (.*) before the column (.*)$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see the column (.*) before the column (.*)$/) do |name1, name2|
+  find('#cm-table1 > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)', :text => name1)
+  find('#cm-table1 > div:nth-child(1) > div:nth-child(3) > div:nth-child(3)', :text => name2)
+end
+
+When(/^I click the button (.*)$/) do |name|
+  first(:css, 'button', :text=> name).click()
+end
+
+Then(/^The cell (.*) should not be visible$/) do |name|
+  all('#cm-table1 .cm-th', :text => /^#{Regexp.escape(name)}$/, :exact => true, :visible => false)
+  all('#cm-table2 .cm-th', :text => /^#{Regexp.escape(name)}$/, :exact => true, :visible => true)
 end
 
 AfterStep('@pause') do
--- features/todo.feature
+++ features/todo.feature
@@ -5,10 +5,30 @@ Feature: Todo
     I want to be able to drag columns, toggle their visibility and resize them
 
     @javascript
-    @pause
     Scenario: Drag one column
         Given I am on the home page
-        And I see the column 2012 and the column 2011
-        When I drag the column 2011 to the column 2012
-        And I release the mouse button
-        Then I should see the column 2012 before the column 2011
+        When  I drag the column 2011 500px to right
+        Then  I should see the column 2012 before the column 2011
+
+    @javascript
+    Scenario: Drag one column back and forth
+        Given I am on the home page
+        When  I drag the column 2011 500px to right
+        And   I drag the column 2011 500px to left
+        Then  I should see the column 2011 before the column 2012
+
+    @javascript
+    Scenario: Hide one column
+        Given I am on the home page
+        When  I click the button Sales Report
+        Then  The cell Sales Report should not be visible
+        And   The cell sales should not be visible
+        And   The cell Info should not be visible
+        And   The cell id should not be visible
+        And   The cell 2011 should not be visible
+        And   The cell 2012 should not be visible
+        And   The cell a should not be visible
+        And   The cell 1º Sem should not be visible
+        And   The cell 2º Sem should not be visible
+        And   The cell 3º Tri should not be visible
+        And   The cell 4º Tri should not be visible
